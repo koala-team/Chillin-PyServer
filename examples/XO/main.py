265ab1bf25d1ed068ae12d6c4292236cc31cf244
AminHP <mdan.hagh@gmail.com>
2017-09-30 16:29:19 +0330

    Add default config path

--- examples/XO/main.py
+++ examples/XO/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # python imports
+import os
 import sys
 
 # chillin imports
@@ -11,6 +12,13 @@ from chillin_server import GameServer, Config
 from game_handler import GameHandler
 
 
-app = GameServer(sys.argv[1])
+config_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "gamecfg.json"
+)
+if len(sys.argv) > 1:
+    config_path = sys.argv[1]
+
+app = GameServer(config_path)
 app.register_game_handler(GameHandler(Config.config))
 app.run()
