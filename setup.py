6c7f01f6533ce9441fc69c43ce27285ca8b10e78
AminHP <mdan.hagh@gmail.com>
2017-10-02 00:49:05 +0330

    Change version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chillin-server',
-    version='1.0.1',
+    version='1.1.0',
     description='Chillin AI Game Framework (Python Server)',
     long_description='',
     author='Koala',
