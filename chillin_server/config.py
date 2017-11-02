bc2d003ceb6186368975dbb57317a41e898de615
AminHP <mdan.hagh@gmail.com>
2017-11-03 00:26:09 +0330

    Add necessary inline configs

--- chillin_server/config.py
+++ chillin_server/config.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # python imports
+import sys
+import re
 import json
 
 # project imports
@@ -12,15 +14,50 @@ class Config(Singleton):
 
     @classmethod
     def initialize(cls, cfg_path):
+        cls._inline_configs = {
+            'net.host': str,
+            'net.port': int,
+            'gui.host': str,
+            'gui.port': int,
+            'gui.replay_dir': str,
+            'gui.replay_filename': str
+        }
+
         with open(cfg_path) as f:
-            cls.config = cls._parse(f.read())
+            cls.config = cls._parse_file(f.read())
+            cls._parse_args(cls.config)
 
 
     @classmethod
-    def _parse(cls, json_text):
+    def _parse_file(cls, json_text):
+        return cls._json_loads(json_text)
+
+
+    @classmethod
+    def _parse_args(cls, config):
+        regex = re.compile("config\.(.+)(\.(.+))*\=(.+)?")
+        for arg in sys.argv:
+            groups = regex.search(arg)
+            if groups:
+                groups = groups.groups()
+                key = groups[0]
+                keys = key.split('.')
+                value = groups[-1]
+                if value:
+                    value = cls._inline_configs[key](groups[-1])
+
+                tmp = config
+                for i in range(len(keys) - 1):
+                    tmp = tmp[keys[i]]
+                tmp[keys[-1]] = value
+
+
+    @classmethod
+    def _json_loads(cls, json_text):
         if PY3:
             return json.loads(json_text)
         return json_loads_byteified(json_text)
+
 
 
 
