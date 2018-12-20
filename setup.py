bfd87bc21b1b63015bcfbc90c2fe21c4df8105c6
AminHP <mdan.hagh@gmail.com>
2018-12-20 15:07:19 +0330

    Add koala-serializer to dependencies

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=[
         'circuits==3.2',
         'pydblite==3.0.4',
+        'koala-serializer==0.6.2',
         'configparser==3.5.0',
         'enum34==1.1.6'
     ],
